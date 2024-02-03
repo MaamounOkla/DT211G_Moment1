2b507510ad0ec76e7aa2c79be9f8f29578b45ff5
Maamoun Okla <mamoun95bo@icloud.com>
2024-02-03 18:52:41 +0100

    Pexon JS färdigt

---
+++ dist/pexon.html.7992.i
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/index.34df27b5.css">
+        <script src="/index.18dbc454.js" defer=""></script>
+        <title>Pexon - DT211G Moment-1</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+               <ul><li><a href="/index.html">Info om uppgiten</a></li>
+               <li><a href="/pictures.html">Bilder</a></li>
+               <li><a href="/description.html">Beskrivning</a></li>
+               </ul>
+            </nav>
+               </header>
+  
+      
+        
+        <main>  
+            <h1>Generare bilder från Pexel.com</h1>
+            <section id="gallery" class="gallery">
+                <!-- JS-Kod -->
+          </section>
+                
+        </main>
+
+    </body>
+
+</html>
---
+++ src/js/main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/*Variables*/
+let API_Key = "563492ad6f9170000100000197b3f2ff2de44d0f8b46fa4f7b03d27b";  
+const gallery = document.getElementById('gallery');
+let fetchLink;
+ 
+ 
+ 
+ 
+
+ 
+/*Functions*/
+window.onload = init();
+function init() {
+    console.log('Getting images from Pexels API..');
+    curatedPhotos();
+}
+/*Fetching API Function*/
+async function fetchAPI(url) {
+    const dataFetch = await fetch(url, {
+        method: 'GET', headers: { Accept: 'application/json', Authorization: API_Key }
+    });
+    const data = await dataFetch.json();
+    return data;
+}
+async function curatedPhotos() {
+    fetchLink = 'https://api.pexels.com/v1/curated?per_page=5&page=2'
+    const data = await fetchAPI(fetchLink);
+    generatePictures(data);
+}
+
+
+function generatePictures(data) {
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement('div');
+        /*Classname for styling */
+        galleryImg.classList.add('gallery-img');
+        galleryImg.innerHTML = `
+
+        <p>By ${photo.photographer}</p>
+        <picture>
+            <source srcset="${photo.src.large}.jpg?as=webp&width=200" type="image/webp">
+            <source srcset="${photo.src.large}.png?as=webp&width=200" type="image/webp">
+            <img src="${photo.src.large}" alt="${photo.alt}">
+        </picture>     
+    `;
+        gallery.appendChild(galleryImg);
+    });
+}
