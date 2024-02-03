13262fbc8f22124177bf4868650f92523e0be327
Maamoun Okla <mamoun95bo@icloud.com>
2024-02-03 19:50:50 +0100

    Ett försök att automatisera bilder från API.

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,37 @@
-'use strict';
+"use strict";
 
 /*Variables*/
-let API_Key = "563492ad6f9170000100000197b3f2ff2de44d0f8b46fa4f7b03d27b";  
-const gallery = document.getElementById('gallery');
+let API_Key = "563492ad6f9170000100000197b3f2ff2de44d0f8b46fa4f7b03d27b";
+const gallery = document.getElementById("gallery");
 let fetchLink;
- 
- 
- 
- 
 
- 
 /*Functions*/
 window.onload = init();
 function init() {
-    console.log('Getting images from Pexels API..');
-    curatedPhotos();
+  console.log("Getting images from Pexels API..");
+  curatedPhotos();
 }
 /*Fetching API Function*/
 async function fetchAPI(url) {
-    const dataFetch = await fetch(url, {
-        method: 'GET', headers: { Accept: 'application/json', Authorization: API_Key }
-    });
-    const data = await dataFetch.json();
-    return data;
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: { Accept: "application/json", Authorization: API_Key },
+  });
+  const data = await dataFetch.json();
+  return data;
 }
 async function curatedPhotos() {
-    fetchLink = 'https://api.pexels.com/v1/curated?per_page=5&page=2'
-    const data = await fetchAPI(fetchLink);
-    generatePictures(data);
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=5&page=2";
+  const data = await fetchAPI(fetchLink);
+  generatePictures(data);
 }
 
-
 function generatePictures(data) {
-    data.photos.forEach(photo => {
-        const galleryImg = document.createElement('div');
-        /*Classname for styling */
-        galleryImg.classList.add('gallery-img');
-        galleryImg.innerHTML = `
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    /*Classname for styling */
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `
 
         <p>By ${photo.photographer}</p>
         <picture>
@@ -45,6 +40,6 @@ function generatePictures(data) {
             <img src="${photo.src.large}" alt="${photo.alt}">
         </picture>     
     `;
-        gallery.appendChild(galleryImg);
-    });
+    gallery.appendChild(galleryImg);
+  });
 }
